ea2eaf39f435cd26b5588ce3d6e4f5b5a131024e
Cooper <eliteswimmer42@gmail.com>
2020-08-16 16:09:53 -0500

    adding and committing my progress on the day. attempting to assign each time-block to a certain time id. i then want to parse the time id against the current time and use that data to re-colorize.

--- script.js
+++ script.js
@@ -8,8 +8,19 @@ displayDate();
 
 
 // recolors the time-blocks based on currentHour
-var currentHour = moment().format('hh');
-var currentHourInt = parseInt(currentHour);
+var coloringHour = moment().format('hh');
+var coloringHourInt = parseInt(currentHour);
+
+// assign each timeblock's data-hour to a solid numerical value
+$("#nineTime").attr("data-hour", moment("9:00 am", "h:mm a").format("hh"));
+$("#tenTime").attr("data-hour", moment("10:00 am", "hh:mm a").format("hh"));
+$("#elevenTime").attr("data-hour", moment("11:00 am", "hh:mm a").format("hh"));
+$("#twelveTime").attr("data-hour", moment("12:00 pm", "hh:mm a").format("hh"));
+$("#thirteenTime").attr("data-hour", moment("1:00 pm", "h:mm a").format("hh"));
+$("#fourteenTime").attr("data-hour", moment("2:00 pm", "h:mm a").format("hh"));
+$("#fifteenTime").attr("data-hour", moment("3:00 pm", "h:mm a").format("hh"));
+$("#sixteenTime").attr("data-hour", moment("4:00 pm", "h:mm a").format("hh"));
+$("#seventeenTime").attr("data-hour", moment("5:00 pm", "h:mm a").format("hh"));
 
 $(document).ready(function() {
     if (timeBlock == currentHour) {
