7f2b8baf961ebf1a654f46ac593874ee28835112
Cooper <eliteswimmer42@gmail.com>
2020-08-17 21:55:51 -0500

    Got a sample div to change colors! Must now refactor hours10-17 in the javascript, making sure they match the var hour9. I must also move the data-hours in the html and add an id to each likened data-hour. after that, i must call each hour into the function to change div colors. Lastly must change the function from if-else to three if statements to make it run properly for each color.

--- script.js
+++ script.js
@@ -6,31 +6,38 @@ function displayDate() {
 displayDate();
 });
 
-
-// recolors the time-blocks based on currentHour
-var coloringHour = moment().format('hh');
-var coloringHourInt = parseInt(currentHour);
-
-// assign each timeblock's data-hour to a solid numerical value
-$("#nineTime").attr("data-hour", moment("9:00 am", "h:mm a").format("hh"));
-$("#tenTime").attr("data-hour", moment("10:00 am", "hh:mm a").format("hh"));
-$("#elevenTime").attr("data-hour", moment("11:00 am", "hh:mm a").format("hh"));
-$("#twelveTime").attr("data-hour", moment("12:00 pm", "hh:mm a").format("hh"));
-$("#thirteenTime").attr("data-hour", moment("1:00 pm", "h:mm a").format("hh"));
-$("#fourteenTime").attr("data-hour", moment("2:00 pm", "h:mm a").format("hh"));
-$("#fifteenTime").attr("data-hour", moment("3:00 pm", "h:mm a").format("hh"));
-$("#sixteenTime").attr("data-hour", moment("4:00 pm", "h:mm a").format("hh"));
-$("#seventeenTime").attr("data-hour", moment("5:00 pm", "h:mm a").format("hh"));
-
-$(document).ready(function() {
-    if (timeBlock == currentHour) {
-        $(this).css("background-color", "red");
+function colorEvents(timeBlock, currentHour, block){
+    if (parseInt(timeBlock) ==parseInt(currentHour)) {
+        $(block).css("background-color", "pink");
     } else {
-        $(this).css("background-color", "grey");
+        $(block).css("background-color", "purple");
     }
-    // nineTime will be recolored
-    // currentHour holds the active time    
-});
+}
+// // recolors the time-blocks based on currentHour
+var coloringHour = moment().format('hh');
+console.log(coloringHour);
+// var coloringHourInt = parseInt(currentHour);
+
+// // assign each timeblock's data-hour to a solid numerical value
+var hour9 = $("#nineTime").attr("data-hour");
+
+
+colorEvents(hour9, coloringHour,"#nineTime");
+// $("#tenTime").attr("data-hour", moment("10:00 am", "hh:mm a").format("hh"));
+// $("#elevenTime").attr("data-hour", moment("11:00 am", "hh:mm a").format("hh"));
+// $("#twelveTime").attr("data-hour", moment("12:00 pm", "hh:mm a").format("hh"));
+// $("#thirteenTime").attr("data-hour", moment("1:00 pm", "h:mm a").format("hh"));
+// $("#fourteenTime").attr("data-hour", moment("2:00 pm", "h:mm a").format("hh"));
+// $("#fifteenTime").attr("data-hour", moment("3:00 pm", "h:mm a").format("hh"));
+// $("#sixteenTime").attr("data-hour", moment("4:00 pm", "h:mm a").format("hh"));
+// $("#seventeenTime").attr("data-hour", moment("5:00 pm", "h:mm a").format("hh"));
+
+
+// $(document).ready(function() {
+//     
+//     // nineTime will be recolored
+//     // currentHour holds the active time    
+// });
 
 
 // saves user-input to the localstorage-- can easily overwrite by changing the input-value and pressing the save button.
